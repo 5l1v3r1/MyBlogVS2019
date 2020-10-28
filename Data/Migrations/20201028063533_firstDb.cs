c9a9eccc8de91788d69e9acaa26011e75ed931e4
Orguntay <67823328+Orguntay@users.noreply.github.com>
2020-10-28 13:46:38 +0300

    migration eklendi

---
+++ Data/Migrations/20201028063533_firstDb.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class firstDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UstCategoryId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    CategoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Points = table.Column<byte>(nullable: false),
+                    Baslik = table.Column<string>(nullable: false),
+                    Ozet = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Etikets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etikets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Etikets_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(nullable: false),
+                    Path = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(nullable: false),
+                    Path = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Videos_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Yorums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UstYorumId = table.Column<int>(nullable: false),
+                    BlogId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    Text = table.Column<string>(nullable: false),
+                    YorumId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Yorums", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Yorums_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Yorums_Yorums_YorumId",
+                        column: x => x.YorumId,
+                        principalTable: "Yorums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryId", "Name", "UstCategoryId" },
+                values: new object[,]
+                {
+                    { 1, null, "Elektronik", 0 },
+                    { 2, null, "Kırtasiye", 0 },
+                    { 3, null, "Bilgisayar", 1 },
+                    { 4, null, "Notebook", 3 },
+                    { 5, null, "Lpatop", 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Blogs",
+                columns: new[] { "Id", "Baslik", "CategoryId", "DateTime", "Ozet", "Points", "Text", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "Elektronik  Başlığımız burda yer alacak", 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Makale özetimiz. Kısaca özet diyebiliriz.", (byte)0, "Unable to create an object of type 'AppDbContext'. For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728", "user777COlombo" },
+                    { 2, "Kırtasiye Makale Başlığımız burda yer alacak", 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Makale özetimiz. Kısaca özet diyebiliriz.", (byte)0, "154 / 5000 AppDbContext türünde bir nesne oluşturulamıyor.Tasarım zamanında desteklenen farklı desenler için bkz. https://go.microsoft.com/fwlink/?linkid=851728", "user777COlombo" },
+                    { 3, "Bigisayar Makale Başlığımız burda yer alacak", 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Makale özetimiz. Kısaca özet diyebiliriz.", (byte)0, "makale içeriği", "user777COlombo" },
+                    { 4, "Notebook Makale Başlığımız burda yer alacak", 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Makale özetimiz. Kısaca özet diyebiliriz.", (byte)0, "mock makale mock mock", "user777COlombo" },
+                    { 5, "Laptop Makale Başlığımız burda yer alacak", 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Makale özetimiz. Kısaca özet diyebiliriz.", (byte)0, "mock makale mock mock", "user777COlombo" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CategoryId",
+                table: "Blogs",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Etikets_BlogId",
+                table: "Etikets",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_BlogId",
+                table: "Images",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_BlogId",
+                table: "Videos",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Yorums_BlogId",
+                table: "Yorums",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Yorums_YorumId",
+                table: "Yorums",
+                column: "YorumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Etikets");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Videos");
+
+            migrationBuilder.DropTable(
+                name: "Yorums");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
